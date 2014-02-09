ad5396807936db1b72781435dae453f520529549
Chor <cl676@cornell.edu>
2014-02-09 03:10:48 -0500

    refactoring

--- client/upload/upload.js
+++ client/upload/upload.js
@@ -50,7 +50,6 @@ var Translator = {
     this.notesBySegmentId = {};
 
     var time = 0;
-    prevNote = null;
 
     for (var i = 0; i < data.length; i++) {
       var noteInfo = data[i];
@@ -65,89 +64,23 @@ var Translator = {
           note: event.noteNumber,
           velocity: event.velocity,
           segmentId: track,
-          // isKeyboardDown: true,
-        };
+        }
 
-        var ignore = false;
         if (event.subtype === "noteOn") {
           note.isKeyboardDown = true;
-
-          // get rid of complicated notes
-          if (prevNote && note.time - prevNote.time < 50 && note.segmentId === prevNote.segmentId) {
-            ignore = true;
-          }
-
-          prevNote = note;
         } 
+        this.notes.push(note);
 
-        if  (!ignore) {
-          this.notes.push(note);
-
-          if (!this.notesBySegmentId[track]) {
-            this.notesBySegmentId[track] = {notes: []};
-          }
-
-          this.notesBySegmentId[track].notes.push(note);
+        if (!this.notesBySegmentId[track]) {
+          this.notesBySegmentId[track] = {notes: []};
         }
+
+        this.notesBySegmentId[track].notes.push(note);
+
       } else {
         // todo: add instrument info
       }
     }
-    this.smartDivide();
-    this.smartSplit();
-  },
-
-  // TODO: move this to the game level
-  smartDivide: function() {
-    var debugSegment = {};
-
-    // analyze each segment and see if we can divide it up further at appropriate points
-    for (segmentId in this.notesBySegmentId) {
-      var notes = this.notesBySegmentId[segmentId].notes;
-      var GOOD_LENGTH = 80;
-      var SAMPLE_LENGTH = 10;
-      var currShift = ''; // TODO: remove this hack for offsetting the trackId because id may conflict
-
-
-      if (notes.length > SAMPLE_LENGTH) {
-        var averageLength = (notes[SAMPLE_LENGTH].time - notes[0].time) / SAMPLE_LENGTH;
-      } else {
-        continue;
-      }
-
-      var count = 0;
-      for (var i = 0; i < notes.length; i++) {
-        var note = notes[i];
-        count++;
-
-        if (!debugSegment[note.segmentId]) {
-          debugSegment[note.segmentId] = []
-        }
-        debugSegment[note.segmentId].push(note);
-
-        // If not too close to the end for a long note after a lengthy segment 
-        if (i < notes.length - GOOD_LENGTH &&
-            notes[i+1].time - note.time > 2 * averageLength && 
-            count > GOOD_LENGTH) {
-
-          notes[i].isEnd = true; // TODO: refactor. This is used when gamifying
-          console.log(i);
-          console.log(notes.length);
-          console.log("===========");
-
-          currShift += 's';
-          count = 0;
-        }
-        // update segmentId everywhere
-        note.segmentId += currShift;
-
-        
-      }
-    }
-  },
-
-  smartSplit: function() {
-    // Split any track that are too complicated into lower and upper tracks
   },
 
   smartShift: function() {
